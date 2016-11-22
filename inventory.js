a3aac84ca3a31dbc21fab4326a2d532b7a49297a
Jacob Froman <jacob.h.froman@gmail.com>
2016-11-22 17:13:09 -0600

    Search results appended to the DOM

--- inventory.js
+++ inventory.js
@@ -1,43 +1,60 @@
-console.log( 'inventory.js sourced' );
+console.log('inventory.js sourced');
 // global inventory
-var inventory=[];
+var inventory = [];
 
-var addItem = function( color, name, size ){
-  console.log( 'adding item: ' + name + ' ' + color + ' ' + size );
-  // create object
-  var newItem = {
-    color: color,
-    name: name,
-    size: size
-  }; // end newItem
-  // push into array
-  inventory.push( newItem );
-  // show updated inventory
-  console.log( 'currentInventory:', inventory );
-} // end addItem
+var addItem = function(color, name, size) {
+    console.log('adding item: ' + name + ' ' + color + ' ' + size);
+    // create object
+    var newItem = {
+        color: color,
+        name: name,
+        size: size
+    }; // end newItem
+    // push into array
+    inventory.push(newItem);
+    // show updated inventory
+    console.log('currentInventory:', inventory);
+}; // end addItem
 
-var searchInventory = function(){
-  console.log( 'in searchInventory' );
-  // get size from user
-  var selectedSize = document.getElementById( 'sizeIn' ).value;
-  // get color from user
-  var selectedColor = document.getElementById( 'colorIn' ).value;
-  console.log( 'looking for something ' + selectedColor + ' and ' + selectedSize );
-  // check if an item fits this description
-  // loop through the inventory array and see if any item matches
-  // array for our results
-  var results = [];
-  for( var i = 0; i < inventory.length; i++ ){
-    // does this item match my description?
-    if( inventory[ i ].color == selectedColor && inventory[ i ].size == selectedSize ){
-      results.push( inventory[ i ] );
+var searchInventory = function() {
+    console.log('in searchInventory');
+    // get size from user
+    var selectedSize = document.getElementById('sizeIn').value;
+    // get color from user
+    var selectedColor = document.getElementById('colorIn').value;
+    console.log('looking for something ' + selectedColor + ' and ' + selectedSize);
+    // check if an item fits this description
+    // loop through the inventory array and see if any item matches
+    // array for our results
+    var results = [];
+    for (var i = 0; i < inventory.length; i++) {
+        // does this item match my description?
+        if (inventory[i].color == selectedColor && inventory[i].size == selectedSize) {
+            results.push(inventory[i]);
+        }
+    } // end for
+    console.log(results.length + ' matches found');
+    console.log(results);
+    return results;
+}; // end searchInventory
+
+var showSearchResults = function() {
+  var results = searchInventory();
+  var resultsList = document.getElementById('resultsList');
+  if(results.length > 0) {
+    var htmlString = "";
+    // Create a list item for each item in the results
+    for (var i = 0; i < results.length; i++) {
+      htmlString += "<li>" + results[i].name + "</li>";
     }
-  }; // end for
-  console.log( results.length + ' matches found' );
-  console.log( results );
-} // end searchInventory
+    // Add the entire list onto the DOM
+    resultsList.innerHTML = htmlString;
+  } else {
+    resultsList.innerHTML = "Found no matches";
+  }
+};
 
 ///////// ADD ITEMS /////////
-addItem( 'blue', 'Smurf', 'Small');
-addItem( 'mermaid treasure', 'Prime Academy', 'Large');
-addItem( 'purple', 'Prince', 'Medium');
+addItem('blue', 'Smurf', 'small');
+addItem('mermaid treasure', 'Prime Academy', 'large');
+addItem('purple', 'Prince', 'medium');
